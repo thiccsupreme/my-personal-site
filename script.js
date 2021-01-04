b9d18d23c452e4a92bfc985fa6d2c34a0868b6fe
thiccsupreme <57068146+thiccsupreme@users.noreply.github.com>
2021-01-04 18:08:12 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+$(document).ready(async function () {
+    $(window).scroll(function () {
+        if (this.scrollY > 20) {
+            $(".navbar").addClass("sticky");
+        } else {
+            $(".navbar").removeClass("sticky");
+        }
+        if(this.scrollY > 500){
+         $(".scroll-up-btn").addClass("show");
+        } else {
+            $(".scroll-up-btn").removeClass("show");
+        }
+    }); 
+
+    $(".scroll-up-btn").click(function() {
+        $("html").animate({scrollTop: 0})
+    })
+
+
+
+    $(".menu-btn").click(function () {
+        $(".navbar .menu").toggleClass("active");
+        $(".menu-btn i").toggleClass("active");
+    })
+})
+
+
+var typed = new Typed(".typing", {
+    strings: ["Developer.", "Sneaker Botter.", "Reseller.", "Designer.", "YouTuber."],
+    typeSpeed: 90, 
+    backSpeed: 60,
+    loop: true
+})
